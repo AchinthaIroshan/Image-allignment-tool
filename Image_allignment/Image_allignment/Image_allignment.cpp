46207c8b21c907e9514cdcae777a90fc336202ea
AchinthaIroshan <achinthahsr@gmail,com>
2017-03-31 21:23:28 +0530

    Added openCV to the project

--- Image_allignment/Image_allignment/Image_allignment.cpp
+++ Image_allignment/Image_allignment/Image_allignment.cpp
@@ -2,10 +2,19 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+using namespace cv;
+//using namespace std;
 
 int main()
 {
+	Mat a = imread("C:\\Users\\\Achintha Iroshan\\Desktop\\LED.jpg");
+	namedWindow("image", WINDOW_AUTOSIZE);
+	imshow("image", a);
+	waitKey(0); //wait infinite time for a keypress
     return 0;
 }
 

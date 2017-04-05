a095e7f8f520b78fa6c855330d376aa06c5f1056
AchinthaIroshan <achinthahsr@gmail,com>
2017-04-05 23:13:08 +0530

    first commit

---
+++ cropAll.m
@@ -0,0 +1,11 @@
+%% Crop the input images in batch
+%  input:   images - A m * n * 3 * x array containing x images
+%  output:  croppedImages - Array of cropped images
+function croppedImages = cropAll(images)
+    len = size(images, 4);
+    for i = 1 : len
+        croppedImages(:, :, :, i) = cropOne(images(:, :, :, i));
+    end
+    figure;
+    imshow(croppedImages(:,:,:,1))
+end
